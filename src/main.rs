7d6abf4d99237c1b41a412e824ad564288601481
talksik <patel.arjun50@gmail.com>
2023-06-25 16:16:16 -0700

    adding in basic flow that is extensible
    now just fill in the blanks
    P

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,34 @@
+use clap::{Parser, ValueEnum};
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+/// Templater is a tool to help you get started quickly configuring cloud apps, libraries,
+/// and frameworks.
+struct Args {
+    /// run `templater list` to see all available options.
+    #[arg(value_enum)]
+    config: Config,
+
+    #[clap(short, long)]
+    /// whether or not to output in the current directory
+    output_file: bool,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum Config {
+    DigitalOceanApp,
+    Kubernetes,
+}
 
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.config {
+        Config::DigitalOceanApp => {
+            let config = std::fs::read_to_string("./src/configs/digitalocean_app_spec.yaml").unwrap();
+            println!("{}", config);
+        },
+        Config::Kubernetes => println!("lib2"),
+        _ => println!("please choose from the following"),
+    }
 }
